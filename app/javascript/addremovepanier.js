f2ff3ec2fb6c73f6ae05fb614432799827323e67
charles noppe <noppe.charles@gmail.com>
2019-06-06 11:33:00 +0200

    addpanier

--- app/javascript/addremovepanier.js
+++ app/javascript/addremovepanier.js
@@ -1,21 +1,34 @@
 const biere = document.querySelectorAll(".card-biere");
 const addtobasket = document.querySelectorAll(".addtobasket");
 const removefrombasket = document.querySelectorAll(".removefrombasket");
+const panierligne = document.querySelector(".panierligne");
 
 
-const majpanier = (x) => {
-  console.log(x);
-};
+const addtopanier = (biereid, bierenom, biereprix, quantite) => {
+  const lignebiere = document.getElementById(`${biereid}`);
+  console.log(lignebiere);
+  const prix = quantite * biereprix;
+
+  if (lignebiere) {
+    lignebiere.querySelector(".bierequantite").innerText = `${quantite}`;
+    lignebiere.querySelector(".biereprix").innerText = `${prix}€`;
+    } else {
+    panierligne.insertAdjacentHTML("beforeend", `<li id="${biereid}" class="list-group-item d-flex justify-content-between align-items-center"><span class="bierenom">${bierenom}</span><span class="badge badge-primary badge-pill bierequantite">${quantite}</span><span class="biereprix">${prix}€</span></li>`)
+  };
+}
 
 
 if (addtobasket) {
-  majpanier("hello");
   let value = 0;
   addtobasket.forEach((button) => {
     button.addEventListener("click", (event) => {
       const inputbasket = button.parentElement.parentElement.getElementsByClassName('form-control')[0];
       value += 1;
       inputbasket.value = value
+      const biereid = button.parentElement.parentElement.parentElement.parentElement.parentElement.dataset.biereid
+      const bierenom = button.parentElement.parentElement.parentElement.parentElement.parentElement.dataset.bierenom
+      const biereprix = button.parentElement.parentElement.parentElement.parentElement.parentElement.dataset.biereprix
+      addtopanier(biereid, bierenom, biereprix, value)
     })
   })
   removefrombasket.forEach((button) => {
